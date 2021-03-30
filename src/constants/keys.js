f257167b9d4fe56fa0a7a439f7b1e0c9235e49d6
k10wl <72667957+k10wl@users.noreply.github.com>
2021-03-30 03:13:51 +0300

    fixed snake collapsing on itself

--- src/constants/keys.js
+++ src/constants/keys.js
@@ -1,31 +1,33 @@
-// const KEY_ARROW_LEFT = 37;
-// const KEY_ARROW_UP = 38;
-// const KEY_ARROW_RIGHT = 39;
-// const KEY_ARROW_DOWN = 40;
-// const KEY_A = 65;
-// const KEY_W = 87;
-// const KEY_D = 68;
-// const KEY_S = 83;
-// const KEY_NP_ARROW_LEFT = 4;
-// const KEY_NP_ARROW_UP = 8;
-// const KEY_NP_ARROW_RIGHT = 6;
-// const KEY_NP_ARROW_DOWN = 2;
-// const KEY_ENTER = 13;
+import store from "../redux-store";
 
 // keybinding
 const bindKeys = (dispatch, left, up, right, down) =>
   document.addEventListener("keydown", (e) => {
-    if (e.key === "a" || e.key === "A" || e.key === "ArrowLeft") {
+    const currentDirection = store.getState().snake.lastStep;
+    if (
+      (e.key === "a" || e.key === "A" || e.key === "ArrowLeft") &&
+      currentDirection !== "RIGHT"
+    ) {
       dispatch(left());
     }
-    if (e.key === "w" || e.key === "W" || e.key === "ArrowUp") {
+    if (
+      (e.key === "w" || e.key === "W" || e.key === "ArrowUp") &&
+      currentDirection !== "DOWN"
+    ) {
       dispatch(up());
     }
-    if (e.key === "d" || e.key === "D" || e.key === "ArrowRight") {
+    if (
+      (e.key === "d" || e.key === "D" || e.key === "ArrowRight") &&
+      currentDirection !== "LEFT"
+    ) {
       dispatch(right());
     }
-    if (e.key === "s" || e.key === "S" || e.key === "ArrowDown") {
+    if (
+      (e.key === "s" || e.key === "S" || e.key === "ArrowDown") &&
+      currentDirection !== "UP"
+    ) {
       dispatch(down());
     }
   });
+
 export default bindKeys;

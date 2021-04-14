e236864af343534d72123c75dbeb1f41fcd9e328
k10wl <72667957+k10wl@users.noreply.github.com>
2021-04-14 23:18:34 +0300

    fixed input bug

--- src/constants/keys.js
+++ src/constants/keys.js
@@ -2,29 +2,25 @@
 const bindKeys = (e, dispatch, left, up, right, down, lastStep) => {
   if (
     (e.key === "a" || e.key === "A" || e.key === "ArrowLeft") &&
-    lastStep !== "RIGHT" &&
-    lastStep !== "LEFT"
+    lastStep !== "RIGHT"
   ) {
     dispatch(left());
   }
   if (
     (e.key === "w" || e.key === "W" || e.key === "ArrowUp") &&
-    lastStep !== "DOWN" &&
-    lastStep !== "UP"
+    lastStep !== "DOWN"
   ) {
     dispatch(up());
   }
   if (
     (e.key === "d" || e.key === "D" || e.key === "ArrowRight") &&
-    lastStep !== "LEFT" &&
-    lastStep !== "RIGHT"
+    lastStep !== "LEFT"
   ) {
     dispatch(right());
   }
   if (
     (e.key === "s" || e.key === "S" || e.key === "ArrowDown") &&
-    lastStep !== "UP" &&
-    lastStep !== "DOWN"
+    lastStep !== "UP"
   ) {
     dispatch(down());
   }
